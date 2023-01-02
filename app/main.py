18ef50295aa68c59ca44016ed18ecebd00140543
dmitriy-lar <larionov.dm2002@gmail.com>
2023-01-02 18:51:24 +0300

    Add metadata to docs page

--- app/main.py
+++ app/main.py
@@ -10,11 +10,26 @@ tags_metadata = [
         "name": "Ingredients",
         "description": "You can manage ingredients there",
     },
+    {
+        'name': 'Users',
+        'description': 'You can register and login there'
+    }
 ]
 
 models.Base.metadata.create_all(engine)
 
-app = FastAPI(redoc_url=None, openapi_tags=tags_metadata)
+app = FastAPI(
+    redoc_url=None,
+    openapi_tags=tags_metadata,
+    title='Book of Recipes',
+    contact={
+        'name': 'Larionov Dmitriy',
+        'email': 'larionov.dm2002@gmail.com'
+    },
+    description="""
+    With this app you can manage recipes
+    """
+)
 
 app.include_router(users.router)
 app.include_router(ingredients.router)
