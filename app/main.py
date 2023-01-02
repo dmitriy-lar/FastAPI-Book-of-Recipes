d6b6cf3c5b419875b2ec59a0f3ca8428bbc1b74b
dmitriy-lar <larionov.dm2002@gmail.com>
2023-01-02 14:29:53 +0300

    create ingredient and category models

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+
 from .routers import ingredients
+from .databases import SessionLocal, engine
+from . import models
+
+models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
---
+++ app/models.py
@@ -0,0 +1,28 @@
+import sqlalchemy
+from sqlalchemy.orm import relationship
+
+from .databases import Base
+
+
+class CategoryIngredientModel(Base):
+    __tablename__ = 'category_ingredients'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
+    title = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
+    description = sqlalchemy.Column(sqlalchemy.TEXT, nullable=True)
+    ingredients = relationship('IngredientModel', back_populates='category')
+
+    def __str__(self):
+        return self.title
+
+
+class IngredientModel(Base):
+    __tablename__ = 'ingredients'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    category_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('category_ingredients.id'))
+    category = relationship('CategoryIngredientModel', back_populates='ingredients')
+
+    def __str__(self):
+        return self.title
